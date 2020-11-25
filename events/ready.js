d01f1cc38a1909829c616958c28676b93e27f2af
skyrawr <6skylarstevens9@gmail.com>
2020-11-25 17:40:18 +0200

    fixes "for the first 15 mins it wont have a status but i cba to do something about it"

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,26 @@
 module.exports = (client) => {
   client.log.info(`Logged in as ${client.user.tag}`);
+  // valid types: PLAYING, STREAMING, LISTENING, WATCHING
+  const statuses = [
+    ['WATCHING', 'gamers'],
+    ['PLAYING', 'Minecraft'],
+    ['WATCHING', 'your mom'],
+    ['PLAYING', 'your mom'],
+    ['WATCHING', 'nyanners vods'],
+    ['WATCHING', 'artemis of the blue clips'],
+    ['LISTENING', 'lemon demon'],
+    ['LISTENING', 'caravan palace'],
+    ['LISTENING', 'pigstep'],
+    ['STREAMING', 'roblox'],
+    ['PLAYING', 'osu!'],
+    ['PLAYING', 'Impact Utility Mod'],
+    ['PLAYING', 'Fortnite'],
+  ];
+  function updateStatus() {
+    const rand = statuses[Math.floor(Math.random() * statuses.length)];
+    client.log.debug(`Setting status to ${rand[0]} ${rand[1]}`);
+    client.user.setPresence({ activity: { name: rand[1], type: rand[0] } });
+  }
+  setTimeout(updateStatus, 1e3); // piss
+  setInterval(updateStatus, 900e3); // 15 minutes
 };
