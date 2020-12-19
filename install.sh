51da553c3e7673a1b3c50caea15e6bd8eb505cff
Christian <cesium2349@protonmail.com>
2020-12-19 15:54:11 -0500

    small formatting stuff and some TODO for install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+# TODO
+# check for more dependencies than just npm, theres also node but im lazy
+# offer to install those dependencies if we can't find them on the system
+# better error messages than "did you make sure you cd'd to the right directory?"
+# maybe prompt for the token and offer to start the systemd service as opposed to a reminder
+# echo some kind of help text if bad flags are passed
+
 # where we'll install the neon service
 SERVICE_DIR="/etc/systemd/system"
 NEON_DIR="$PWD"
-
 
 # ARGUMENTS
 # -u will set it to execute it as a different user than the current one
@@ -34,12 +40,11 @@ else
     exit 1
 fi
 
-
 # lol im so bad at naming variables
 THINGY="\
 [Unit]
 Description=Neon Discord bot
-
+dependencies
 [Service]
 ExecStart=runuser -l $USER -c 'cd $NEON_DIR && npm run start'
 
@@ -51,8 +56,8 @@ WantedBy=multi-user.target\
 echo "Creating $SERVICE_DIR/neon.service that will run as user $USER for $NEON_DIR"
 sudo sh -c "echo \"$THINGY\" > $SERVICE_DIR/neon.service"
 sudo chmod +x $SERVICE_DIR/neon.service
-
+# create the config file
 echo "Creating config file"
 cp -n $NEON_DIR/defaultconfig.json $NEON_DIR/config.json
-
+# and we're done!
 echo "Finished installing. Remember to tell systemctl to enable neon, and remember to give the config a token"
