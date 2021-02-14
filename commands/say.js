d933c81a20117db38fba8e13023c75f46834cd27
Christian <cesium2349@protonmail.com>
2021-02-14 16:16:08 -0500

    only took me four days to notice
    it just didnt start becasue i forgot a (
    looks like theres more in this commit but i dont care what lol

--- commands/say.js
+++ commands/say.js
@@ -1,4 +1,5 @@
 exports.run = (client, message) => {
+  client.log.info(`${message.author.name} made me say ${message.content.substring(client.config.prefix.length + 3)}`);
   // send the same message it received, but with the length of the prefix cut off plus three more characters
   message.channel.send(message.content.substring(client.config.prefix.length + 3))
   // delete the message right after we do what we were told so it looks like its just the bot talking
