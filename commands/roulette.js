dd430c6eefa3bcfb4e7a9ef8af5ac3a7e342e90e
Christian <cesium2349@protonmail.com>
2021-04-10 00:07:37 -0400

    polish (the verb not the ethnic group)

--- commands/roulette.js
+++ commands/roulette.js
@@ -6,14 +6,15 @@ exports.run = (client, message) => {
   let odds = getRandomInt(5); // because 0 counts and 0..5 is six total numbers
   if (odds == 0) {
     if (message.member.kickable) {
-      message.channel.send('bang bang bang fucker');
+      message.react('💥');
+      message.channel.send('bang!');
       message.member.kick('russian roulette loser');
     }
     else {
-      message.channel.send('bang bang bang but i cant kick you so could you please leave the server yourself and pretend it was a kick');
+      message.channel.send('this is the part where youd get kicked but i dont have perms to do that');
     }
   }
-  else message.channel.send('click!');
+  else message.react('👍');
 };
 
 exports.info = {
